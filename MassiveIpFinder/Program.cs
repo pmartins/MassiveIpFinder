bc3eca404811324dee7a4a772f910bd6a13de574
Pedro Martins <pedro.mart@gmail.com>
2015-04-10 23:48:06 +0100

    First commit

---
+++ MassiveIpFinder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace MassiveIpFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file =
+               new System.IO.StreamReader(args[0]);
+
+            string line = null;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                IPAddress[] addresslist = null;
+
+                try
+                { addresslist = Dns.GetHostAddresses(line); }
+                catch { }
+
+                if (addresslist != null)
+                {
+                    foreach (IPAddress theaddress in addresslist)
+                    {
+                        Console.WriteLine(theaddress.ToString());
+                    }
+                    line = null;
+                }
+            }
+        }
+    }
+}
